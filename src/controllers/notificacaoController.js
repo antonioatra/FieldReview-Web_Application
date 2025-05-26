9fa217f495ec0dad18d918f94e15b9e779ab109c
Diego Figueiredo <diego.silva@sou.inteli.edu.br>
2025-05-26 15:46:01 -0300

    feat/adicionado seção para criar notificações

---
+++ src/controllers/notificacaoController.js
@@ -0,0 +1,19 @@
+const Notificacao = require("../models/notificacaoModels");
+const UsuarioNotificacao = require("../models/usuarioNotificacao");
+
+exports.store = async (req, res) => {
+    const { titulo, descricao, usuarios } = req.body;
+
+    const notificacao = await Notificacao.create({ titulo, descricao });
+
+    if (usuarios && usuarios.length > 0) {
+        for (const id_usuario of usuarios) {
+            await UsuarioNotificacao.create({
+                id_usuario,
+                id_notificacao: notificacao.id
+            });
+        }
+    }
+
+    res.redirect("/notificacoesModels");
+};
--- src/models/usuarioNoticacao.js
+++ src/models/usuarioNoticacao.js
@@ -1,3 +1,30 @@
 const db = require("../config/database");
 
-a
+
+
+
+module.exports = {
+    async create(data) {
+        return db.insert("usuario_notificacao", data);
+    },
+
+    async findUsuariosByNotificacao(id_notificacao) {
+        return db.query(
+            "SELECT u.* FROM usuario u INNER JOIN usuario_notificacao un ON u.id = un.id_usuario WHERE un.id_notificacao = ?",
+            [id_notificacao]
+        );
+    },
+
+    async deleteByNotificacao(id_notificacao) {
+        return db.query("DELETE FROM usuario_notificacao WHERE id_notificacao = ?", [id_notificacao]);
+    }
+};
+
+
+
+
+
+
+
+
+
