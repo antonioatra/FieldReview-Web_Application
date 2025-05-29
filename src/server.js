7d0fc59ee85dbb90fa64a052480b88098ba08f69
Gabriel W. Bartmanovicz <obielwb@gmail.com>
2025-05-29 14:56:07 -0300

    feat: add initial ejs

---
+++ src/server.js
@@ -0,0 +1,58 @@
+require('dotenv').config();
+const express = require('express');
+
+const path = require('path');
+const { title } = require('process');
+
+const app = express();
+
+app.use(express.json());
+
+// Configuração do EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rota inicial
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+
+app.get('/search', (req, res) => {
+  res.render('search', {
+    results: [
+      {
+        title: 'Exemplo de Título',
+        description:
+          'adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboreis nis ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui off',
+      },
+    ],
+  });
+});
+
+app.get('/trail/:id', (req, res) => {
+  const trailId = req.params.id;
+  res.render('trail', {
+    id: trailId,
+    title: 'Título da Trilha',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  });
+});
+
+// Iniciar servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
+
+module.exports = app;
