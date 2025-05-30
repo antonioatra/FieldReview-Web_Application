ceeeea3dafb5020d9392474f4ad69949519c46f7
T0nhao <antonio.andre@sou.inteli.edu.br>
2025-05-30 14:05:29 -0300

    fix: Padronizando o código e adicionando comentários no model de opcoes

--- src/models/opcoes.js
+++ src/models/opcoes.js
@@ -1,28 +1,28 @@
 const pool = require("../config/database");
 
-module.exports ={
-    async create(data) {
+module.exports ={ // Cria as funções do model
+    async create(data) { // Função que cria a opção
         const query = "INSERT INTO opcoes (texto, eh_correta) VALUES($1, $2)";
-        const values = [data.texto, data.eh_correta];
+        const values = [data.text, data.isCorrect];
 
         return pool.query(query, values);
     },
 
-    async findById(id) {
+    async findById(id) { // Função que pega opção por id
         const query = "SELECT * FROM opcoes WHERE id = $1";
         const values = [id];
 
         return pool.query(query, values);
     },
 
-    async delete(id) {
+    async delete(id) { // Função que deleta opção 
         const query = "DELETE FROM opcoes WHERE id = $1";
         const values = [id];
 
         return pool.query(query, values);
     },
 
-    async update(id) {
+    async update(id) { // Função que atualiza opção
         const query = "UPDATE opcoes SET texto = $1, eh_correta = $2";
         const values = [data.text, data.isCorrect];
 
