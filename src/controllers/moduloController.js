45d8f2b7e2bef00c9d3ac81c7ac5ba3e65eb8602
T0nhao <antonio.andre@sou.inteli.edu.br>
2025-05-29 21:14:36 -0300

    fix: Adicionando comentários e padronizando o view e controller de modulo

--- src/controllers/moduloController.js
+++ src/controllers/moduloController.js
@@ -1,34 +1,34 @@
-const Modulo = require("../models/modulo");
+const Module = require("../Modules/modulo"); // chama o model da entidade módulo
 
-exports.store = async(req, res) => {
-    await Modulo.create(req.body);
-    res.redirect("/module"); 
+exports.store = async(req, res) => { // Chama a função que cria módulos
+    await Module.create(req.body); // Espera que seja criada
+ res.redirect("/module"); //Redireciona para o endpoint de módulo
 };
 
-exports.showById =async(req, res) => {
+exports.showById =async(req, res) => { // Chama a função que pega os módulos por id
     const {id} = req.params;
-    const modulo = await Modulo.findById(id);
-    res.json(modulo);
+    const mod = await Module.findById(id);
+    res.json(mod);
 };
 
-exports.showByTraildId = async(req, res) => {
-    const modulo = await Modulo.findByTrailId(req.body);
-    res.json(modulo)
+exports.showByTraildId = async(req, res) => { // Chama a função que pega módulos pelo id da trilha 
+    const mod = await Module.findByTrailId(req.body);
+    res.json(mod)
 };
 
-exports.showByUserId = async(req, res) => {
-    const modulo = await Modulo.findByUserId(req.body);
-    res.json(modulo);
+exports.showByUserId = async(req, res) => { // Chama a função que pega módulos a pelo id do usuário
+    const mod = await Module.findByUserId(req.body);
+    res.json(mod);
 };
 
-exports.update = async(req, res) => {
+exports.update = async(req, res) => {// Chama a função que atualiza módulos
     const {id} = req.params;
-    await Modulo.update(id, req.body);
+    await Module.update(id, req.body);
     res.redirect("/module");
 };
 
-exports.destroy = async (req, res) =>  {
+exports.destroy = async (req, res) =>  { // Chama a função que deleta módulos
     const {id} = req.params;
-    await Modulo.delete(id);
+    await Module.delete(id);
     res.redirect("/module");
 };
