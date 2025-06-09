6b787b237dc202906151f960ad19dd78ad03fd1b
Gabriel W. Bartmanovicz <obielwb@gmail.com>
2025-06-09 08:14:48 -0300

    chore: remove unecessary console.logs

--- src/controllers/searchController.js
+++ src/controllers/searchController.js
@@ -1,14 +1,9 @@
 const Search = require('../models/search');
 
 exports.search = async (req, res) => {
-  console.log('Received search request with term:', req.body);
   try {
     const helpResults = await Search.helpTextualSearch(req.body.searchTerm);
     const moduleResults = await Search.moduleTextualSearch(req.body.searchTerm);
-    console.log('Search results:', {
-      help: helpResults,
-      modules: moduleResults,
-    });
 
     res.status(200).json({
       help: helpResults,
